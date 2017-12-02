16e9a148f36b937f3e3461ac08c55da0137cf32b
alexied <alexie.dariciuc@student.upt.ro>
2017-12-02 21:17:38 +0200

    added slow working method

--- opencv_ex/client.py
+++ opencv_ex/client.py
@@ -1,17 +1,22 @@
 import cv2
-import numpy as np
+import numpy
 import socket
-import sys
+import time
 import pickle
 import struct
-import time
-import matplotlib.pyplot as plt
-cap=cv2.VideoCapture(0)
+import mss
+import sys
+import screeninfo
+
+m = screeninfo.get_monitors()
+monitor = {'top': 0, 'left': 0, 'width': 640, 'height': 480}
 
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('192.168.0.101',8089))
-while True:
-    ret,frame=cap.read()
-    data = pickle.dumps(frame)
-    print len(data)
-    clientsocket.sendall(struct.pack("L", len(data))+data)
+with mss.mss() as sct:
+    while True:
+        last_time = time.time()
+        img = numpy.array(sct.grab(monitor))
+        data = pickle.dumps(img) #this is slowing me down a lot
+        clientsocket.sendall(struct.pack("L", len(data))+data)
+        print('fps: {0}'.format(1 / (time.time() - last_time)))
