b5281878b350c8e6bf66ee390180ca09bf3afd5c
alexied <alexie.dariciuc@student.upt.ro>
2017-12-03 00:42:16 +0200

    speedup

--- opencv_ex/client.py
+++ opencv_ex/client.py
@@ -7,6 +7,8 @@ import struct
 import mss
 import sys
 import screeninfo
+import io
+import zlib
 
 m = screeninfo.get_monitors()
 monitor = {'top': 0, 'left': 0, 'width': 640, 'height': 480}
@@ -16,7 +18,21 @@ clientsocket.connect(('192.168.0.101',8089))
 with mss.mss() as sct:
     while True:
         last_time = time.time()
-        img = numpy.array(sct.grab(monitor))
-        data = pickle.dumps(img) #this is slowing me down a lot
-        clientsocket.sendall(struct.pack("L", len(data))+data)
+        grabs = sct.grab(monitor)
+        img = numpy.array(grabs)
+        payload = img.tostring()
+        print(len(payload))
+        res = clientsocket.send(str(len(payload)))
+        if res != len(str(len(payload))):
+            print("err")
+            print(res)
+        acknowledge = clientsocket.recv(1)
+        if int(acknowledge) == 1 :
+            clientsocket.send(payload)
+        else:
+            print("ackg failed")
+        while True:
+            sack = clientsocket.recv(1)
+            if int(sack) == 1:
+                break
         print('fps: {0}'.format(1 / (time.time() - last_time)))
--- opencv_ex/server.py
+++ opencv_ex/server.py
@@ -19,18 +19,19 @@ print 'Socket now listening'
 conn,addr=s.accept()
 
 data = ""
-payload_size = struct.calcsize("L")
 while True:
-    while len(data) < payload_size:
-        data += conn.recv(16384)
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += conn.recv(16384)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-
-    frame=pickle.loads(frame_data)
-    cv2.imshow('frame',frame)
+    size = conn.recv(20)
+    size = 1228800
+    print(size)
+    if int(size) > 0:
+        conn.send(str(1))
+    else:
+        conn.send(str(0))
+    data = ""
+    while len(data) < int(size):
+      data += conn.recv(4096)
+    print(len(data))
+    conn.send(str(1))
+    img = np.fromstring(data,dtype=np.uint8).reshape(480,640,4)
+    cv2.imshow('frame',img)
     cv2.waitKey(1)
